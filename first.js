9e5662b77619b14e49ac77fccd50bda7a110181e
0122345 <118376302+0122345@users.noreply.github.com>
2022-12-22 03:18:00 +0200

    some codes added as well as removed

---
+++ first.js
@@ -0,0 +1,15 @@
+{/* <script>
+function myFunction() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav")
+   {
+    x.className += " responsive"
+  } 
+  else {
+    x.className = "topnav"
+  }
+ 
+</script> */}
+
+
+
