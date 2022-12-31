325d72325a1a8900c7f19daac19a77da551e4bde
0122345 <118376302+0122345@users.noreply.github.com>
2022-12-31 23:46:22 +0200

    changing form validatiion

--- first.js
+++ first.js
@@ -1,5 +1,56 @@
-function myFunction(x) {
-  x.classList.toggle("change");
+ const form = document.getElementById('form');
+ const name = document.getElementById('name');
+ const email = document.getElementById('email');
+ const subject = document.getElementById('subject');
+ const msgbox = document.getElementById('msgbox');
+
+ form.addEventListener('submit',(e) =>{
+    e.preventDefault();
+
+    checkInputs();
+ });
+  
+ function checkInputs(){
+  // get values from input fields
+ const nameValue = name.value.trim(); 
+const  emailValue = email.value.trim();
+const subjectValue = subject.value.trim();
+const msgboxValue = msgbox.value.trim();
+   
+
+if(nameValue == ''){
+
+  setErrorFor(name,'please your name is important');
+}else{
+
+  setSuccessFor(name);
 }
+ }
+
+ if(emailValue ==''){
+ setErrorFor(email,'please your email is essential');
+ }else if(!isEmail(emailValue)){
+  setErrorFor(email,'please enter valid email');
+ }else{
+  setSuccessFor(email);
+ }
+
+ function setErrorFor(input, message){
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+
+  small.innerText = message;
+
+  formControl.className = 'form-control error';
+ }
+ function setSuccessFor(input){
+ const formControl = inpt.parentElement;
+ formControl.className = 'form-control success';
+ }
+
+function isEmail(email){
+  return   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
 
 
