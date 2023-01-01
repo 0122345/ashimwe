127aaf0e1ec70d14438eb7881f064c74e5bfa5be
0122345 <118376302+0122345@users.noreply.github.com>
2023-01-02 00:47:24 +0200

    working on valiation

--- first.js
+++ first.js
@@ -10,7 +10,7 @@
     checkInputs();
  });
   
- function checkInputs(){
+ function  send(){
   // get values from input fields
  const nameValue = name.value.trim(); 
 const  emailValue = email.value.trim();
@@ -21,18 +21,39 @@ const msgboxValue = msgbox.value.trim();
 if(nameValue == ''){
 
   setErrorFor(name,'please your name is important');
-}else{
-
-  setSuccessFor(name);
+  
 }
- }
+else{
+
+  setErrorFor(name,'you can continue');
+}
+ 
+if(subjectValue == ''){
+
+  setErrorFor(subject,'please can you specify a subject');
+  
+}
+else{
+
+  setErrorFor(subject,'you can continue');
+}
+
+if(msgboxValue == ''){
+
+  setErrorFor(msgbox,'can you leave us a comment please!?');
+  
+}
+else{
+
+  setErrorFor(msgbox,'you can continue');
+}
 
  if(emailValue ==''){
  setErrorFor(email,'please your email is essential');
  }else if(!isEmail(emailValue)){
   setErrorFor(email,'please enter valid email');
  }else{
-  setSuccessFor(email);
+  setErrorFor(email,'you can now continue!');
  }
 
  function setErrorFor(input, message){
@@ -51,6 +72,13 @@ if(nameValue == ''){
 function isEmail(email){
   return   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+// if (validation()) // Calling validation function
+// {
+// document.getElementById("form_id").submit(); //form submission
+// alert(" Name : " + name + " n Email : " + email + " n Form Id : " + document.getElementById("form_id").getAttribute("id") + "nn Form Submitted Successfully......");
+// }
+// }
 
 
+}
 
