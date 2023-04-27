f9323c0dd0dce2f67d7e5f4830146e74837468aa
0122345 <118376302+0122345@users.noreply.github.com>
2023-04-27 17:32:27 +0200

    modifying

--- first.js
+++ first.js
@@ -1,4 +1,98 @@
 // localStorage.setItem("name","domestic"); akako karakora
+function validate(){
+         
+  var name = document.getElementById("name")
+ var email = document.getElementById("email")
+ var subject = document.getElementById("subject")
+ var msgbox = document.getElementById("msgbox")
+  
+
+var x=document.myform.email.value;  
+  var atposition=x.indexOf("@");  
+  var dotposition=x.lastIndexOf(".");   
+
+
+  if( name.value.trim()==""){
+   alert("please enter your name")
+    name.style.border = "solid 3px red";
+    return false;
+  }
+
+
+  else if(email.value.trim()== ""){
+   alert("please enter your email")
+    email.style.border = "solid 3px red";
+     return false;
+  }
+
+ 
+
+  else   if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+    alert("Please enter a valid e-mail address ! ");  
+    return false;  
+    }
+    
+ else if(subject.value.trim()== ""){
+  subject.style.border = "solid 3px red";
+     return false;
+  }
+
+ 
+  else if(msgbox.value.trim()== ""){
+    msgbox.style.border = "solid 3px red";
+     return false;
+  }
+  else if(msgbox.value.trim().length<20){
+    alert("please enter atleast 20 characters")
+     return false;
+  }
+
+
+ else if( email.value.trim()!=""){
+    email.style.border = "solid 3px green";
+    email.style.transitionDelay( 25);
+    return true;
+  }
+
+ else if( subject.value.trim()!=""){
+    subject.style.border = "solid 3px green";
+    subject.style.transitionDelay( 25);
+    return true;
+  }
+    else if( name.value.trim()!=""){
+    name.style.border = "solid 3px green";
+    name.style.transitionDelay( 25);
+    return true;
+  }
+
+else{
+ alert("your post was received is unsuccessful received!")
+//  name.style.border = "solid 3px green";
+//  email.style.border = "solid 3px green";
+//  subject.style.border = "solid 3px green";
+//  msgbox.style.border = "solid 3px green";
+  return false;
+}
+  }
+  function con() { 
+
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const subject = document.getElementById("subject").value;
+const msgbox = document.getElementById("msgbox").value;
+
+localStorage.setItem("name",name);
+localStorage.setItem("email",email);
+localStorage.setItem("subject",subject);
+localStorage.setItem("msgbox",msgbox);
+ 
+
+};
+
+
+
+
+
   
 let section = document.querySelectorAll("section");
 let menu = document.querySelectorAll("navi a button");
@@ -43,10 +137,4 @@ window.addEventListener("scroll", reveal);
 reveal();
 
 
-function click(){
-  
-  // onclick="document.getElementById('a5').innerHTML =  button.style.color ="white" 
-
-
-}
-
+ 
